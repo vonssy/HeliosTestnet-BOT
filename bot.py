114c9fc37de03546793d5f1cb5942a0c5b8a34fd
vonssy <kak.reyvaldizakaria@gmail.com>
2025-06-26 01:33:41 +0700

    Fix Insufficient Gas

--- bot.py
+++ bot.py
@@ -28,11 +28,11 @@ class Helios:
         self.RPC_URL = "https://testnet1.helioschainlabs.org/"
         self.HLS_CONTRACT_ADDRESS = "0xD4949664cD82660AaE99bEdc034a0deA8A0bd517"
         self.VALIDATION_CONTRACT_ADDRESS = [
-            # {"Moniker": "Helios-Hedge", "Contract Address": "0x007a1123a54cdD9bA35AD2012DB086b9d8350A5f"},
-            # {"Moniker": "Helios-Peer", "Contract Address": "0x72a9B3509B19D9Dbc2E0Df71c4A6451e8a3DD705"},
-            # {"Moniker": "Helios-Unity", "Contract Address": "0x7e62c5e7Eba41fC8c25e605749C476C0236e0604"},
+            {"Moniker": "Helios-Hedge", "Contract Address": "0x007a1123a54cdD9bA35AD2012DB086b9d8350A5f"},
+            {"Moniker": "Helios-Peer", "Contract Address": "0x72a9B3509B19D9Dbc2E0Df71c4A6451e8a3DD705"},
+            {"Moniker": "Helios-Unity", "Contract Address": "0x7e62c5e7Eba41fC8c25e605749C476C0236e0604"},
             {"Moniker": "Helios-Supra", "Contract Address": "0x882f8A95409C127f0dE7BA83b4Dfa0096C3D8D79"},
-            # {"Moniker": "Helios-Inter", "Contract Address": "0xa75a393FF3D17eA7D9c9105d5459769EA3EAEf8D"}
+            {"Moniker": "Helios-Inter", "Contract Address": "0xa75a393FF3D17eA7D9c9105d5459769EA3EAEf8D"}
         ]
         self.BRIDGE_ROUTER_ADDRESS = "0x0000000000000000000000000000000000000900"
         self.DELEGATE_ROUTER_ADDRESS = "0x0000000000000000000000000000000000000800"
@@ -253,8 +253,10 @@ class Helios:
             if allowance < amount_to_wei:
                 approve_data = token_contract.functions.approve(spender, amount_to_wei)
 
+                latest_block = web3.eth.get_block("latest")
+                base_fee = latest_block.get("baseFeePerGas", 0)
                 max_priority_fee = web3.to_wei(1.111, "gwei")
-                max_fee = max_priority_fee
+                max_fee = base_fee + max_priority_fee
 
                 approve_tx = approve_data.build_transaction({
                     "from": address,
@@ -316,8 +318,10 @@ class Helios:
 
             calldata = "0x7ae4a8ff" + encoded_data
 
+            latest_block = web3.eth.get_block("latest")
+            base_fee = latest_block.get("baseFeePerGas", 0)
             max_priority_fee = web3.to_wei(1.111, "gwei")
-            max_fee = max_priority_fee
+            max_fee = base_fee + max_priority_fee
 
             tx = {
                 "to": self.BRIDGE_ROUTER_ADDRESS,
